8d4abe358652b61c6d2055d6a837a008abd449e1
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-09-26 13:41:26 -0300

    Criando botões com icones

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
 import TelaHome from "./Componentes/TelaHome";
 import TelaDescricao from "./Componentes/TelaDescricao";
 import TelaRotas from "./Componentes/TelaRotas";
@@ -12,12 +14,29 @@ const Abas = createBottomTabNavigator();
 export default function App () {
   return (
     <NavigationContainer >
-      <Abas.Navigator>
-        <Abas.Screen name="Home" component = { TelaHome }/>
+      <Abas.Navigator screenOptions={ menuNav } >
+        
+        <Abas.Screen name="Home" component = { TelaHome } options={home}/>
         <Abas.Screen name="Telas" component = { TelaDescricao } />
         <Abas.Screen name="Rotas" component = { TelaRotas } />
         <Abas.Screen name="Biblioteca" component = { TelaBiblioteca } />
+      
       </Abas.Navigator>
     </NavigationContainer>
   )
 };
+
+const menuNav = {
+  tabBarActiveTintColor: "#8B14FD",
+  tabBarInactiveTintColor: "#59B2FF",
+  tabBarLabelPosition:"below-icon",
+  tabBarActiveBackgroundColor: "#a8a8a8",
+  tabBarInactiveBackgroundColor: "#CEDBD1",
+}
+
+const home = {
+  tabBarIcon:({color})=>(
+    <MaterialCommunityIcons name="shield-home-outline" size={24} color={color} />
+  )
+}
+
